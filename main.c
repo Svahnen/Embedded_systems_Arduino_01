5e0a52f97c37fbc4bfac9a16a84f23f9996ada6a
Svahnen <svahn.jonny@gmail.com>
2022-03-02 22:05:43 +0100

    Set PB1 to low

--- main.c
+++ main.c
@@ -3,4 +3,6 @@
 #include "serial.h"
 
 void main(void) {
+    DDRB |= (1 << PB1);    // Set PB1 (Pin9, blue LED on the shield) in DDRB (Port B Data Direction Register) to 1, this makes PB1 and output
+    PORTB &= ~(1 << PB1);  // Set PB1 in PORTB to 0 (low) since we need to connect to GND to turn on
 }
