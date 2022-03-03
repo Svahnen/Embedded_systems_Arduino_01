0d5dcdbf59490f5289abf93f424958b61d0c9822
Svahnen <svahn.jonny@gmail.com>
2022-03-03 12:42:59 +0100

    Create a bad timer

--- main.c
+++ main.c
@@ -1,8 +1,27 @@
 #include <avr/io.h>
+#include <stdbool.h>
 
 #include "serial.h"
+#define BLUE PB1
 
-void main(void) {
-    DDRB |= (1 << PB1);    // Set PB1 (Pin9, blue LED on the shield) in DDRB (Port B Data Direction Register) to 1, this makes PB1 an output
-    PORTB &= ~(1 << PB1);  // Set PB1 in PORTB to 0 (low) since we need to connect to GND to turn on
+bool running = true;
+
+int main(void) {
+    DDRB |= (1 << BLUE);    // Set PB1 (Pin9, blue LED on the shield) in DDRB (Port B Data Direction Register) to 1, this makes PB1 an output
+    PORTB &= ~(1 << BLUE);  // Set PB1 in PORTB to 0 (low) since we need to connect to GND to turn on
+    uint8_t timerOverflowCount = 0;
+    TCNT0 = 0x00;  // Clear the counter
+    TCCR0B = (1 << CS00) | (1 << CS02);
+
+    while (1) {
+        while ((TIFR0 & 0x01) == 0) {  // Monitor the TOV0 flag in the TIFR0 register to check if the timer has over-flowed
+        }
+        TCNT0 = 0x00;  // Clear the counter
+        TIFR0 = 0x01;  // clear timer1 overflow flag
+        timerOverflowCount++;
+        if (timerOverflowCount >= 100) {
+            PORTB ^= (1 << BLUE);  // Invert the state of BLUE
+            timerOverflowCount = 0;
+        }
+    }
 }
