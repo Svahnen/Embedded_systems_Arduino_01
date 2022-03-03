34452944e5572e22ef05fad4665af911ab411029
Svahnen <svahn.jonny@gmail.com>
2022-03-03 10:00:19 +0100

    Fix spelling

--- main.c
+++ main.c
@@ -3,6 +3,6 @@
 #include "serial.h"
 
 void main(void) {
-    DDRB |= (1 << PB1);    // Set PB1 (Pin9, blue LED on the shield) in DDRB (Port B Data Direction Register) to 1, this makes PB1 and output
+    DDRB |= (1 << PB1);    // Set PB1 (Pin9, blue LED on the shield) in DDRB (Port B Data Direction Register) to 1, this makes PB1 an output
     PORTB &= ~(1 << PB1);  // Set PB1 in PORTB to 0 (low) since we need to connect to GND to turn on
 }
