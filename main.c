db381b4351f8ded5e8f5d7922312356fc9d7dcc4
Svahnen <svahn.jonny@gmail.com>
2022-03-03 15:38:43 +0100

    Compiling but not sending

--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 
 #include "serial.h"
 #define BLUE PB1
+#define BAUD 9600                               // define baud
+#define BAUDRATE ((F_CPU) / (BAUD * 16UL) - 1)  // set baud rate value for UBRR
 
 bool running = true;
 
@@ -11,8 +13,13 @@ int main(void) {
     DDRB |= (1 << BLUE);    // Set PB1 (Pin9, blue LED on the shield) in DDRB (Port B Data Direction Register) to 1, this makes PB1 an output
     PORTB &= ~(1 << BLUE);  // Set PB1 in PORTB to 0 (low) since we need to connect to GND to turn on
 
+    UBRR0H = (BAUDRATE >> 8);                                  // shift the register right by 8 bits
+    UBRR0L = BAUDRATE;                                         // set baud rate
+    UCSR0B |= (1 << TXEN0);                                    // enable RX
+    UCSR0C |= (1 << UMSEL00) | (1 << UCSZ00) | (1 << UCSZ01);  // 8bit data format
     while (running) {
-        PORTB ^= (1 << BLUE);  // Invert the state of BLUE
-        _delay_ms(500);
+        while (!(UCSR0A & (1 << UDRE0))) {
+        }            // wait while register is free
+        UDR0 = 'a';  // load data in the register
     }
 }
--- serial.c
+++ serial.c
@@ -4,13 +4,6 @@
 #include <util/delay.h>
 
 void uart_init(void) {
-    /*Set baud rate */
-    UBRR0H = (unsigned char)(ubrr >> 8);
-    UBRR0L = (unsigned char)ubrr;
-    Enable receiver and transmitter * /
-        UCSR0B = (1 << RXEN0) | (1 << TXEN0);
-    /* Set frame format: 8data, 2stop bit */
-    UCSR0C = (1 << USBS0) | (3 << UCSZ00);
 }
 
 void uart_putchar(char chr) {
