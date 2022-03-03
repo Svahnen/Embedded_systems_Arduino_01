c5bda9528628bd2b9e6dfbabcc848f54f7ca3d1a
Svahnen <svahn.jonny@gmail.com>
2022-03-03 16:21:38 +0100

    Move functions to serial.c

--- main.c
+++ main.c
@@ -1,9 +1,6 @@
 #include <avr/io.h>
 #include <stdbool.h>
 #include <util/delay.h>
-#define F_CPU 16000000UL
-#define BAUD 9600
-#include <util/setbaud.h>
 
 #include "serial.h"
 #define BLUE PB1
@@ -13,21 +10,9 @@ bool running = true;
 int main(void) {
     DDRB |= (1 << BLUE);    // Set PB1 (Pin9, blue LED on the shield) in DDRB (Port B Data Direction Register) to 1, this makes PB1 an output
     PORTB &= ~(1 << BLUE);  // Set PB1 in PORTB to 0 (low) since we need to connect to GND to turn on
-    // Baud Rate Register uses 2 bytes, H and L for all its settings, some unused bytes in H needs to be set to 0 for things to work
-    UBRR0H = UBRRH_VALUE;  // Right shifts 8 times
-    UBRR0L = UBRRL_VALUE;
-
-// Look for Double Speed (asynchronous mode only)
-#if USE_2X
-    UCSR0A |= _BV(U2X0);
-#else
-    UCSR0A &= ~(_BV(U2X0));
-#endif
-
-    UCSR0C = _BV(UCSZ01) | _BV(UCSZ00);  // 8-bit data
-    UCSR0B = _BV(RXEN0);                 // Enable RX
+    uart_init();
     while (running) {
-        loop_until_bit_is_set(UCSR0A, UDRE0); /* Wait until data register empty. */
-        UDR0 = 'a';
+        uart_putchar('c');
+        _delay_ms(1000);
     }
 }
--- serial.c
+++ serial.c
@@ -2,12 +2,29 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
+#define F_CPU 16000000UL
+#define BAUD 9600
+#include <util/setbaud.h>
 
 void uart_init(void) {
+    // Baud Rate Register uses 2 bytes, H and L for all its settings, some unused bytes in H needs to be set to 0 for things to work
+    UBRR0H = UBRRH_VALUE;  // Right shifts 8 times
+    UBRR0L = UBRRL_VALUE;
+
+// Look for Double Speed (asynchronous mode only)
+#if USE_2X
+    UCSR0A |= _BV(U2X0);
+#else
+    UCSR0A &= ~(_BV(U2X0));
+#endif
+
+    UCSR0C = _BV(UCSZ01) | _BV(UCSZ00);  // 8-bit data
+    UCSR0B = _BV(RXEN0) | _BV(TXEN0);    /* Enable RX and TX */
 }
 
 void uart_putchar(char chr) {
-    // while (!(UCSRnA & (1 << UDREn))) {}
+    loop_until_bit_is_set(UCSR0A, UDRE0); /* Wait until data register empty. */
+    UDR0 = chr;
 }
 void uart_putstr(const char *str) {
     // UDRn = str;
