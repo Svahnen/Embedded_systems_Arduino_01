38b937f2474932bf94d7bed3f6976b755bc7e75e
Svahnen <svahn.jonny@gmail.com>
2022-03-03 15:16:29 +0100

    Initial test

--- serial.c
+++ serial.c
@@ -1,4 +1,25 @@
+#include "serial.h"
+
 #include <avr/io.h>
 #include <util/delay.h>
 
-#include "serial.h"
+void uart_init(void) {
+    /*Set baud rate */
+    UBRR0H = (unsigned char)(ubrr >> 8);
+    UBRR0L = (unsigned char)ubrr;
+    Enable receiver and transmitter * /
+        UCSR0B = (1 << RXEN0) | (1 << TXEN0);
+    /* Set frame format: 8data, 2stop bit */
+    UCSR0C = (1 << USBS0) | (3 << UCSZ00);
+}
+
+void uart_putchar(char chr) {
+    // while (!(UCSRnA & (1 << UDREn))) {}
+}
+void uart_putstr(const char *str) {
+    // UDRn = str;
+}
+
+char uart_getchar(void);
+
+void uart_echo(void);
