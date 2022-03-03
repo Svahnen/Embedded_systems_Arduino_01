bcb01c34079c4db8d660de51d951213535b68ec8
Svahnen <svahn.jonny@gmail.com>
2022-03-03 15:51:39 +0100

    First TX sent

--- main.c
+++ main.c
@@ -1,25 +1,31 @@
 #include <avr/io.h>
 #include <stdbool.h>
 #include <util/delay.h>
+#define F_CPU 16000000UL
+#define BAUD 9600
+#include <util/setbaud.h>
 
 #include "serial.h"
 #define BLUE PB1
-#define BAUD 9600                               // define baud
-#define BAUDRATE ((F_CPU) / (BAUD * 16UL) - 1)  // set baud rate value for UBRR
 
 bool running = true;
 
 int main(void) {
     DDRB |= (1 << BLUE);    // Set PB1 (Pin9, blue LED on the shield) in DDRB (Port B Data Direction Register) to 1, this makes PB1 an output
     PORTB &= ~(1 << BLUE);  // Set PB1 in PORTB to 0 (low) since we need to connect to GND to turn on
+    UBRR0H = UBRRH_VALUE;
+    UBRR0L = UBRRL_VALUE;
 
-    UBRR0H = (BAUDRATE >> 8);                                  // shift the register right by 8 bits
-    UBRR0L = BAUDRATE;                                         // set baud rate
-    UCSR0B |= (1 << TXEN0);                                    // enable RX
-    UCSR0C |= (1 << UMSEL00) | (1 << UCSZ00) | (1 << UCSZ01);  // 8bit data format
+#if USE_2X
+    UCSR0A |= _BV(U2X0);
+#else
+    UCSR0A &= ~(_BV(U2X0));
+#endif
+
+    UCSR0C = _BV(UCSZ01) | _BV(UCSZ00); /* 8-bit data */
+    UCSR0B = _BV(RXEN0) | _BV(TXEN0);   /* Enable RX and TX */
     while (running) {
-        while (!(UCSR0A & (1 << UDRE0))) {
-        }            // wait while register is free
-        UDR0 = 'a';  // load data in the register
+        loop_until_bit_is_set(UCSR0A, UDRE0); /* Wait until data register empty. */
+        UDR0 = 'a';
     }
 }
